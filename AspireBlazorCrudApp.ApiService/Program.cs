e0c849c6244fa0a5fb1b43f6d5cd9d8474731da3
naveedkapoorkhan <naveedkapoorkhan@gmail.com>
2025-09-07 12:00:22 +0500

    Add project files.

---
+++ AspireBlazorCrudApp.ApiService/Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire client integrations.
+builder.AddServiceDefaults();
+//i added it why
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add services to the container.
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseExceptionHandler();
+//i add it why 
+//app.MapControllers();
+//i did it why
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // 👈 makes Swagger at "/"
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.MapDefaultEndpoints();
+
+app.Run();
+
---
+++ AspireBlazorCrudApp.Model/Entities/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspireBlazorCrudApp.Model.Entities
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Quantity { get; set; }
+       public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
